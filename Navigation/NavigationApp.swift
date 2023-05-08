7c5eed14a4d83f18b95cdd3df43a24d74d62c100
o-ifeanyi <onuifeanyi95@gmail.com>
2023-05-08 09:48:24 +0100

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Ifeanyi Onuoha on 08/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
